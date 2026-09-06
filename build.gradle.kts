f79fb09352eb178e8d7edb4f7f168803ae1491e0
ssnagin <s_lstoo@bk.ru>
2026-09-06 17:39:00 +0300

    project is set to Java

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
+    id("java")
 }
 
 group = "org.seitmolabs"
@@ -10,12 +10,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
